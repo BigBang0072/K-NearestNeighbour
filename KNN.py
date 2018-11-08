2a08088c46ceda054d614ec78d569249081efbd8
Abhinav <abhinavkumar.sieger@gmail.com>
2018-11-08 23:25:01 +0530

    Added the data parsing and cleaning function.

---
+++ KNN.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def data_parser(data16_fname,data17_fname,loc_fname):
+    '''
+    This function will load the data in pandas dataframe and then
+    remove the null entries.
+    USAGE:
+        INPUT:
+            data16_fname   :  2016 pollution data filename
+            data17_fname   :  2017 pollution data filename
+            loc_fname  :  the location file
+    '''
+    #Reading the dataframe and cleaning them
+    df16=pd.read_csv(data16_fname)
+    _remove_nan_from_df(df16)
+
+    #Reading the 2017 dataframe
+    df17=pd.read_csv(data17_fname)
+    _remove_nan_from_df(df17)
+
+    #Rerading and cleaning the stations dataframe
+    dfLoc=pd.read_csv(loc_fname)
+    _remove_nan_from_df(dfLoc)
+
+    return df16,df17,dfLoc
+
+def _remove_nan_from_df(df):
+    '''
+    This function will remove the nan values from the dataframe
+    '''
+    #Getting the mean of each column of full year
+    mean_dict=df.describe().loc['mean'].to_dict()
+
+    #Filling the missing values with the mean of each columns
+    df.fillna(value=mean_dict,inplace=True)
+    print "Read and cleaned the dataframe\n",df.head()
+
+if __name__=='__main__':
+    data16_fname='dataset/madrid_2016.csv'
+    data17_fname='dataset/madrid_2017.csv'
+    loc_fname='dataset/stations.csv'
+
+    #Cleaning the dataset
+    data_parser(data16_fname,data17_fname,loc_fname)
